d5df17c7ab46929a48a3fcbf27ca3a7fe5a25d02
Shrutika <shrutika37tarapure@gmail.com>
2021-06-08 23:58:35 +0530

    commit
    Signed-off-by: Shrutika <shrutika37tarapure@gmail.com>

--- CheckedUncheckedTest.java
+++ CheckedUncheckedTest.java
@@ -1,12 +1,15 @@
+//example 
+
 package com.cg.demo.ex;
 
 public class CheckedUncheckedTest {
+	
 
 	public static void main(String[] args) {
 
 		System.out.println("Start");
 
-// 		System.out.println(0 / 0); // ArithmeticException - unchecked exception 
+ 	//	System.out.println(0 / 0); // ArithmeticException - unchecked exception 
 
 //		Thread.sleep(1000); // InterruptedException - checked exception 
 
